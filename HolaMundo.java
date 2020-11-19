06976e47b437b0c770edb2ec47f219809765479e
fernandof <51677207+fernandof14@users.noreply.github.com>
2020-11-19 15:33:38 -0300

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -5,7 +5,7 @@ import java.awt.Toolkit;
 
 public class HolaMondo {
     public static void main(String [] args){
-        System.out.println("Hola mundo");
+        System.out.println("Hola Mundo!!!!");
         System.out.println("Fernando Fernandez");
         
     }
