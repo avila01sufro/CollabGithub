8c1bac7205df522a710c506485379da0ec7d0a3f
fernandof <51677207+fernandof14@users.noreply.github.com>
2020-11-19 15:24:46 -0300

    Update HolaMondo.java

--- HolaMondo.java
+++ HolaMondo.java
@@ -7,8 +7,7 @@ public class HolaMondo {
     public static void main(String [] args){
         System.out.println("Hola mundo");
         System.out.println("Fernando Fernandez");
-        System.out.flush();
-
+        
     }
     
 }
